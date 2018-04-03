abe25174854426ce065824542ed187f3b3a92254
Jacob Lurye <jlurye@college.harvard.edu>
2018-04-03 09:40:08 -0400

    First pass on reduce; un-idiomatic Option use.

--- src/main/scala/scalambda/Interpreter.scala
+++ src/main/scala/scalambda/Interpreter.scala
@@ -17,17 +17,17 @@ class LambdaCalcInterpreter {
   }
 
   /** Finds the set of free variables in a lambda calculus expression. */
-  def findFVs(e: LExp,
+  def findFVs(exp: LExp,
                       fvs: Set[String] = Set.empty,
                       cvs: Set[String] = Set.empty): Set[String] = {
-    e match {
+    exp match {
       case LVar(x) => if (cvs(x)) Set.empty else fvs + x
       case LLam(x, e) => findFVs(e, fvs, cvs + x)
       case LApp(e1, e2) => findFVs(e1, fvs, cvs) union findFVs(e2, fvs, cvs)
     }
   }
 
-  /** Replace all instances of x in e1 with e2. */
+  /** Replaces all instances of x in e1 with e2. */
   def subst(e1: LExp, x: String, e2: LExp): LExp = {
     e1 match {
       case LVar(s) => if (x == s) e2 else e1
@@ -46,6 +46,35 @@ class LambdaCalcInterpreter {
             LLam(freshVar, subst(subst(e, s, LVar(freshVar)), x, e2))
           }
         }
+    }
+  }
+
+  /** Takes one small step according to the lambda calc semantics */
+  def reduce(exp: LExp): Option[Lexp] = {
+    exp match {
+      case LVar(_) => None
+      case LLam(x, e) => {
+        reduce(e) match {
+          case Some(e1) => Some(LLam(x, e1))
+          case None => None
+        }
+      }
+      case LApp(e1, e2) => {
+        e1 match {
+          case LLam(x, t1) => Some(subst(t1, x, e2))
+          case _ => {
+            reduce(e1) match {
+              case Some(e1r) => Some(LApp(e1r, e2))
+              case None => {
+                reduce(e2) match {
+                  case Some(e2r) => Some(LApp(e1, e2r))
+                  case None => None
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
